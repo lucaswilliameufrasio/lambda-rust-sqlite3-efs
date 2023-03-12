95992a2fbea19267c6b037e757fb552598a186e1
Lucas William <lucaswilliameufrasio@gmail.com>
2023-03-12 04:44:03 -0300

    yolo push

--- src/main.rs
+++ src/main.rs
@@ -175,11 +175,3 @@ impl IntoResponse for MyError {
     }
 }
 
-/// Utility function for mapping any error into a `500 Internal Server Error`
-/// response.
-fn internal_error<E>(err: E) -> (StatusCode, String)
-where
-    E: std::error::Error,
-{
-    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
-}
