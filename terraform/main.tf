3317b485a6a5f259441bd51c9250d3c9b0c91679
Lucas William <lucaswilliameufrasio@gmail.com>
2023-07-12 01:10:39 -0300

    feat: add aws provider region

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region for all resources."
+  type        = string
+  default     = "us-east-1"
+}
+
 locals {
   prefix = "lambda-rust-sqlite3-efs"
 
